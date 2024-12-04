9b9add601e600ac0b6d23077f18b378656d29ef6
Batikan-Kutluer <60034174+Batikan-Kutluer@users.noreply.github.com>
2024-12-04 17:31:33 +0300

    Upgraded to NextJS 15 with a fresh start. To be continued...

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-## Başlarken
-
-Merhaba, bu projede kendi websitemi kodladım.
-
-## Getting Started (EN)
-
-Hello, in this project i coded my own website.
+To be continued...
--- mongo.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-
-const { MongoClient } = require('mongodb');
-
-const { uri } = process.env;
-
-const Mongo = new MongoClient(uri);
-
-module.exports = Mongo;
